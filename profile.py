c88a307c72dea43c7909742c25fadfbc041538d1
Suguru Kato <salmon.bioinformatics@gmail.com>
2016-04-26 15:33:44 +0900

    Modify profile.py

--- profile.py
+++ profile.py
@@ -3,22 +3,20 @@
 import time
 import math
 import numpy
+from functools import partial
 from ecell4 import *
 
-NUM_STEPS = 10000
-RUN_LIMIT = 500.0 # sec
 
 radius = 5e-9
-
 with species_attributes():
     A | { 'radius':  str(radius), 'D': '1.0e-12' }
-
 model = get_model()
 
-def print_usage(methods, factories):
-    print("USAGE: python profile.py [" + "|".join(methods) + "] [" + "|".join(factories) + "] <int>")
+ns = numpy.logspace(1.5, 4.5, 7).astype(int)
 
-# per simulation time
+#NUM_STEPS = 10000
+NUM_STEPS = 100
+RUN_LIMIT = 500.0 # sec
 def measure_run_time(simulator, steps):
     elapsed = 0
     start = time.time()
@@ -28,17 +26,11 @@ def measure_run_time(simulator, steps):
         if (elapsed >= RUN_LIMIT):
             elapsed *= float(steps) / (i+1)
             break
-
+    print("steps:{}, t:{}".format(steps, simulator.t()))
     return elapsed / simulator.t()
 
 def num_partitions(L, N):
     return int(min(L/(2*radius), max(3, cbrt(N))))
-
-def create_factory(constructor, L, num, partition, *args):
-    if partition:
-        m = num_partitions(L, num)
-        return constructor(Integer3(m, m, m), rng)
-    return constructor(*args)
 
 def create_world(factory, L, num):
     world = factory.create_world(Real3(L, L, L))
@@ -46,53 +38,51 @@ def create_world(factory, L, num):
     world.add_molecules(Species('A'), num)
     return world
 
-def profile_fixed_volume(ns, constructor, partition, scaled_step, *args):
-    edge_length = 3.42e-6 # V = 40fL
+def profile(edge_length_method, rng, constructor, scaled_step, *args):
     elapsed_time = []
     for num in ns:
-        factory = create_factory(constructor, edge_length, num, partition, *args)
+        edge_length = edge_length_method(num)
+        factory = constructor(edge_length, num, rng, *args)
         world = create_world(factory, edge_length, num)
-        s = factory.create_simulator(model, world)
-        num_steps = num * NUM_STEPS if scaled_step else NUM_STEPS
-        try:
-            elapsed = measure_run_time(s, num_steps)
-        except:
-            elapsed_time.append(0.0)
-            break
-        elapsed_time.append(elapsed)
+        simulator = factory.create_simulator(model, world)
+        steps_per_cycle = num if scaled_step else 1
+        elapsed_time.append(measure_run_time(simulator, int(steps_per_cycle * NUM_STEPS)))
     return elapsed_time
 
-def profile_fixed_concentration(ns, constructor, partition, scaled_step, *args):
-    elapsed_time = []
-    for num in ns:
-        volume = num/60.0*1.0e-18
-        edge_length = cbrt(volume)
-        factory = create_factory(constructor, edge_length, num, partition, *args)
-        world = create_world(factory, edge_length, num)
-        s = factory.create_simulator(model, world)
-        num_steps = num * NUM_STEPS if scaled_step else NUM_STEPS
-        try:
-            elapsed = measure_run_time(s, num_steps)
-        except:
-            elapsed_time.append(0.0)
-            break
-        elapsed_time.append(elapsed)
-    return elapsed_time
+def get_edge_length_with_40fL(num):
+    return 3.42e-6
+
+def get_edge_length_with_100nM(num):
+    return cbrt(num/60.0*1.0e-18)
+
+def create_partitioned_constructor(constructor):
+    def make_instance(L, num, rng, *args):
+        m = num_partitions(L, num)
+        return constructor(Integer3(m,m,m), *(args + (rng,)))
+    return make_instance
+
+def create_non_partitioned_constructor(constructor):
+    def make_instance(L, num, rng, *args):
+        return constructor(*(args + (rng,)))
+    return make_instance
+
+
+factories = {
+    "spatiocyte" : [create_non_partitioned_constructor(spatiocyte.SpatiocyteFactory), False, radius],
+    "egfrd"      : [create_partitioned_constructor(egfrd.EGFRDFactory),               True],
+    "egfrd.bd"   : [create_partitioned_constructor(egfrd.BDFactory),                  False],
+    "bd.bd"      : [create_partitioned_constructor(bd.BDFactory),                     False],
+    "meso"       : [create_non_partitioned_constructor(meso.MesoscopicFactory),       True,  100e-9]
+}
+profile_methods = {
+    "vol"  : partial(profile, get_edge_length_with_40fL),
+    "conc" : partial(profile, get_edge_length_with_100nM)
+}
+
+def print_usage(methods, factories):
+    print("USAGE: python profile.py [" + "|".join(methods) + "] [" + "|".join(factories) + "] <int>")
 
 if __name__ == '__main__':
-    rng = GSLRandomNumberGenerator()
-    factories = {
-        "spatiocyte" : [spatiocyte.SpatiocyteFactory, False, False, radius, rng],
-        "egfrd"      : [egfrd.EGFRDFactory, True, True, rng],
-        "egfrd.bd"   : [egfrd.BDFactory, True, True, rng],
-        "bd.bd"      : [bd.BDFactory, True, True, rng],
-        "meso"       : [meso.MesoscopicFactory, False, True, 100e-9, rng]
-    }
-    profile_methods = {
-        "vol"  : profile_fixed_volume,
-        "conc" : profile_fixed_concentration
-    }
-
     import sys
     argc = len(sys.argv)
     if argc < 3:
@@ -102,11 +92,9 @@ if __name__ == '__main__':
     method = sys.argv[1]
     system = sys.argv[2]
 
+    rng = GSLRandomNumberGenerator()
     if argc >= 4:
-        seed = int(sys.argv[3])
-    else:
-        seed = 0
-    rng.seed(seed)
+        rng.seed(int(sys.argv[3]))
 
     if method not in profile_methods:
         print("Invalid method")
@@ -118,8 +106,7 @@ if __name__ == '__main__':
         print_usage(profile_methods.keys(), factories.keys())
         sys.exit(3)
 
-    ns = numpy.power(10, numpy.arange(1.5, 5.0, 0.5)).astype(int)
-    elapsed_time = profile_methods[method](ns, *(factories[system]))
-    with open("tsv/%s_fixed_%s_%d.tsv" % (system, method, seed), 'w') as f:
+    elapsed_time = profile_methods[method](rng, *factories[system])
+    with open("tsv/%s_fixed_%s.tsv" % (system, method), 'w') as f:
         for (num, time) in zip(ns, elapsed_time):
             print >> f,  num, time
