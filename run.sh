8103e404baa430ddbfa1d36f236db23e893a7764
Suguru Kato <salmon.bioinformatics@gmail.com>
2016-04-26 15:17:54 +0900

    Modify run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,10 @@ methods=(vol conc)
 factories=(spatiocyte egfrd egfrd.bd bd.bd meso)
 
 seed=$1
-if [ -z "$seed" ]; then
-    seed=0
-fi
 
 for m in ${methods[@]}; do
-    for f in ${factories[@]}; do
-        nohup python profile.py $m $f $seed > log/${m}_${f}.out &
-    done
+  for f in ${factories[@]}; do
+    nohup python profile.py $m $f $seed > log/${m}_${f}.out &
+  done
 done
 
